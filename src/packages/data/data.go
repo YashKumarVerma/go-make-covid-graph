ce26978376dfecb3fc2ade5fb4510e51ce5da726
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-11 16:22:18 +0530

    package to fetch live COVID data

---
+++ src/packages/data/data.go
@@ -0,0 +1,45 @@
+package data
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type responseStructure struct {
+	country     string
+	countryCode string
+	province    string
+	city        string
+	cityCode    string
+	lat         string
+	lon         string
+	Cases       int
+	status      int
+	Date        string
+}
+
+// Fetch latest data from COVID api
+func Fetch() []float64 {
+	var cases []float64
+	resp, err := http.Get("https://api.covid19api.com/country/india/status/confirmed")
+
+	if err != nil {
+		panic("Unable to connect to server")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic("Invalid body received")
+	}
+
+	var response []responseStructure
+	json.Unmarshal([]byte(body), &response)
+
+	for _, item := range response {
+		cases = append(cases, float64(item.Cases))
+	}
+
+	return cases
+}
